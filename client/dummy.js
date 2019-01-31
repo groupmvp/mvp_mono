556648aca8def46293cc361fca8c18524de4aa61
coffeegoddd <dbeaubrown@gmail.com>
2019-01-30 17:34:20 -0800

    [client] added click event and sending selection to socket server

--- client/dummy.js
+++ client/dummy.js
@@ -1,11 +1,32 @@
 
 var socket = io.connect('http://localhost:3000'); 
 
+const selectionMap = {
+  1: 'rock',
+  2: 'paper',
+}
+
+let state = 0;
 // if we get an "info" emit from the socket server then console.log the data we receive
 const game = document.getElementById('game');
-socket.on('info', function (data) {
+socket.on('playerHasConnected', function (data) {
+    state = data.playerNumber;
+
     console.log(data);
     let node = document.createElement('div');
     node.textContent = data.msg;
     game.appendChild(node);
 });
+
+const button = document.getElementById('selection');
+
+const sendSelection = () => {
+  socket.emit('clicked', { selection: selectionMap[state] });
+}
+
+// button.onclick = sendSelection;
+
+socket.on('selection', (data) => {
+  console.log('from socket, on selection event -->', data);
+});
+
