d78d6820d2a015adfee947e6e375d591782fd2d6
coffeegoddd <dbeaubrown@gmail.com>
2019-01-30 19:15:21 -0800

    [client] handling results object from server after event is emmitted

--- client/dummy.js
+++ client/dummy.js
@@ -7,11 +7,12 @@ const selectionMap = {
 }
 
 let state = 0;
+let socketId = '';
 // if we get an "info" emit from the socket server then console.log the data we receive
 const game = document.getElementById('game');
 socket.on('playerHasConnected', function (data) {
     state = data.playerNumber;
-
+    socketId = data.socketId;
     console.log(data);
     let node = document.createElement('div');
     node.textContent = data.msg;
@@ -21,12 +22,18 @@ socket.on('playerHasConnected', function (data) {
 const button = document.getElementById('selection');
 
 const sendSelection = () => {
-  socket.emit('clicked', { selection: selectionMap[state] });
+  socket.emit('clicked', {
+    socketId,
+    playerNumber: state, 
+    selection: selectionMap[state], 
+  });
 }
-
-// button.onclick = sendSelection;
 
 socket.on('selection', (data) => {
   console.log('from socket, on selection event -->', data);
+});
+
+socket.on('bothPlayersReady', (data) => {
+  console.log('ready -->', data);
 });
 
