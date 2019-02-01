c4c666eaea24878ee21f91ac472fc6770bf31ae9
coffeegoddd <dbeaubrown@gmail.com>
2019-02-01 11:13:37 -0800

    attempted to pull front-end to make waiting list component

--- client/dummy.js
+++ client/dummy.js
@@ -11,15 +11,10 @@ let socketId = '';
 let queue = [];
 // if we get an "info" emit from the socket server then console.log the data we receive
 const game = document.getElementById('game');
-<<<<<<< HEAD
 socket.on('playerHasConnected', function (data) {
     state = data.playerNumber;
     socketId = data.socketId;
     console.log(data);
-=======
-socket.on('info', function (data) {
-    console.log(data, 'helloo');
->>>>>>> 76eda5f26ad23d2e342f9207eb2898d170580f56
     let node = document.createElement('div');
     node.textContent = data.msg;
     game.appendChild(node);
