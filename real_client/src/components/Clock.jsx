76eda5f26ad23d2e342f9207eb2898d170580f56
Sris Moparthy <smopart@gmail.com>
2019-01-31 17:48:51 -0800

    changed clock

--- real_client/src/components/Clock.jsx
+++ real_client/src/components/Clock.jsx
@@ -1,29 +1,3 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import style from "./styles/Clock.css";
-
-// class Clock extends React.Component {
-//     constructor(props) {
-//       super(props);
-//       this.state = {
-//         count: 30
-//       }
-//     }
-    
-//     componentDidMount() {
-      
-//     }
-
- 
-//     render() {
-//         return(
-//             <div className={style.clock}>
-
-//             </div>            
-//         )
-//     }
-// }
-// export default Clock;
 import React from 'react';
 import ReactDOM from 'react-dom';
 import style from "./styles/Clock.css";
@@ -33,7 +7,7 @@ class Clock extends React.Component {
     constructor(props) {
       super(props);
       this.state = {
-          date: 30
+          time: 30
       };
     }
   
@@ -50,7 +24,7 @@ class Clock extends React.Component {
   
     tick() {
       this.setState({
-        date: this.state.date-1
+        time: this.state.time-1
       });
     }
   
@@ -58,7 +32,7 @@ class Clock extends React.Component {
       return (
         <div className = {style.clock}>
 
-          <h2>{this.state.date}</h2>
+          <h2>{this.state.time}</h2>
         </div>
       );
     }
