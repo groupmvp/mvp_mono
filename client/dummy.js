00daddde764259d951880404a69344b471a10900
coffeegoddd <dbeaubrown@gmail.com>
2019-02-01 17:03:16 -0800

    added dockerfiles to each server

--- client/dummy.js
+++ client/dummy.js
@@ -9,14 +9,23 @@ const selectionMap = {
 let state = 0;
 let socketId = '';
 let queue = [];
+let hasUsername = false;
+let username = '';
+
+const handlePrompt = () => {
+  username = prompt('Please choose a username rookie', 'Your not gud');
+};
 // if we get an "info" emit from the socket server then console.log the data we receive
 const game = document.getElementById('game');
 socket.on('playerHasConnected', function (data) {
+    if (!username) {
+      handlePrompt();
+    }
     state = data.playerNumber;
     socketId = data.socketId;
     console.log(data);
     let node = document.createElement('div');
-    node.textContent = data.msg;
+    node.textContent = data.msg + ` ${username}`;
     game.appendChild(node);
 });
 
